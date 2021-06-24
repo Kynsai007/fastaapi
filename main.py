1e36f393c66e6fbd4a42d338b49e44bafc23d33c
Kynsai Iangrai <kynsai.iangrai@datasemantics.in>
2021-06-24 23:47:04 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import spacy
+from pydantic import BaseModel
+app = FastAPI()
+
+
+class ModelData(BaseModel):
+    document: str
+
+class TestData(BaseModel):
+    id:int
+    message:str
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# @app.post("/predict")
+# async def predict(d:ModelData):
+#     nlp_updated = spacy.load('tmp_model')
+#     results = nlp_updated(d.document)
+#     data = {}
+#     for ent in results.ents:
+#         data.update({
+#             ent.label_: ent.text
+#         })
+#     return data
+
+@app.post("/test")
+def test(data:TestData):
+    return {'message':data.message,'id':data.id}
